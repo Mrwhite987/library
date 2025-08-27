978faea27de8473cf60f5c5a0c163f40e07e1328
Mrwhite987 <Mrwhite987@hotmail.com>
2025-08-27 23:45:34 +0800

    Adds book library management features
    Initializes the core logic for a book library application.
    
    Introduces a `Book` constructor for creating book objects with properties like title, author, pages, read status, and a unique ID. Implements functions to add new books to a library array, dynamically display them on the page, and remove them.
    
    Adds an HTML dialog form for users to input new book details. Sets up event listeners to handle opening/closing the dialog, submitting new book data, and removing existing books from the display.

---
+++ main.js
@@ -0,0 +1,110 @@
+let myLibrary = [];
+// let displayedBookUUID = [];
+
+function Book(title, author, pages, read = false) {
+  if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor");
+  }
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.uuid = crypto.randomUUID();
+  this.info = function () {
+    if (read) {
+      let readStatus = "not read yet";
+    } else {
+      let readStatus = "have read";
+    }
+    const info = `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`;
+    return info;
+  };
+}
+
+function addBookToLibrary(title, author, pages, read = false) {
+  // take params, create a book then store it in the array
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  // console.log(newBook.uuid);
+}
+
+addBookToLibrary("Dune", "Frank Herbert", 688, false);
+addBookToLibrary("Neuromancer", "William Gibson", 271, false);
+addBookToLibrary("The Martian", "Andy Weir", 369, false);
+
+displayBooks(myLibrary);
+
+function displayBooks(bookArray) {
+  const displayContainer = document.querySelector(".book-shelf");
+  displayContainer.textContent = "";
+  for (let book of bookArray) {
+    const bookCard = createBookCard(book);
+    displayContainer.append(bookCard);
+  }
+}
+
+function createBookCard(book) {
+  const bookCard = document.createElement("div");
+  bookCard.classList.add("book-card");
+  const bookTitle = document.createElement("h1");
+  bookTitle.textContent = book.title;
+  const bookAuthor = document.createElement("p");
+  bookAuthor.textContent = book.author;
+  const bookPages = document.createElement("p");
+  bookPages.textContent = `${book.pages} pages`;
+  const bookReadStatus = document.createElement("p");
+  if (book.read) {
+    bookReadStatus.textContent = "Read";
+  } else {
+    bookReadStatus.textContent = "Unread";
+  }
+  const bookRemoveBtn = document.createElement("button");
+  bookRemoveBtn.textContent = "Remove";
+  bookRemoveBtn.className = "remove-book";
+  bookRemoveBtn.id = book.uuid;
+  bookCard.append(
+    bookTitle,
+    bookAuthor,
+    bookPages,
+    bookReadStatus,
+    bookRemoveBtn
+  );
+  return bookCard;
+}
+
+const openAddNewBookDialogBtn = document.getElementById("open-dialog-button");
+const cancelBtn = document.getElementById("cancel-btn");
+const addBookDialog = document.getElementById("add-book");
+const newBookForm = document.getElementById("new-book-form");
+
+// Open dialog
+openAddNewBookDialogBtn.addEventListener("click", () =>
+  addBookDialog.showModal()
+);
+
+// Cancel closes dialog
+cancelBtn.addEventListener("click", () => addBookDialog.close());
+
+// Handle form submission
+newBookForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(newBookForm);
+  const book = Object.fromEntries(formData.entries());
+  let read = false;
+  if (book["read-status"] == "read") {
+    read = true;
+  }
+  addBookToLibrary(book.title, book.author, Number(book.pages), read);
+  displayBooks(myLibrary);
+  addBookDialog.close();
+});
+
+const displayContainer = document.querySelector(".book-shelf");
+
+displayContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("remove-book")) {
+    const uuid = e.target.id;
+    myLibrary = myLibrary.filter((book) => book.uuid !== uuid);
+    displayBooks(myLibrary);
+  }
+});
